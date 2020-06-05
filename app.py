47a93b48bdf2fc40d4b74a4563bd4332eaa1c06a
Behrad Koohy <43882475+behradkoohy@users.noreply.github.com>
2020-06-05 23:31:08 +0100

    added email generation

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, jsonify, request
+from emails import draftEmails
+from urllib import parse
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def landing():
-    return render_template('landing.html')
+	if request.method == 'GET':
+		return render_template('landing.html')
+	else:
+		name = request.form['name']
+		postcode = request.form['postcode']
+		postcode = postcode.replace(" ", "")
+		emails = draftEmails(name, postcode)
+		a = [{'email': (e.email), 'subject': parse.quote(e.subject), 'body': parse.quote(e.body)} for e in emails]
+		# return jsonify(a)
+		return render_template('emails.html', emails=a)
