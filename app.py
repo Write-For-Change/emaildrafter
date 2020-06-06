fa28b2585a8a0d50275c2f0a2af1284f6aefed92
Behrad Koohy <43882475+behradkoohy@users.noreply.github.com>
2020-06-06 14:54:09 +0100

    added new csv that Django made, looks sick just need to change it to find the background :(

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, jsonify, request
+from flask import Flask, render_template, flash, jsonify, request, url_for
 from emails import draftEmails
 from urllib import parse
 app = Flask(__name__)
