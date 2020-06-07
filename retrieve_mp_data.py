c8ae7a11c369bd66a081b1a1ff0975b5f65be054
Tai Kirby <tai.m.kirby@gmail.com>
2020-06-07 14:26:29 +0100

    Updated csv to include today's MPs and retrieve_mp_data so it returns the preferred names of MPs

--- retrieve_mp_data.py
+++ retrieve_mp_data.py
@@ -7,7 +7,7 @@ def retrieve_mp_data():
 
     MPdata = {}
 
-    MPurl = "http://lda.data.parliament.uk/commonsmembers.json?_view=members&_pageSize=5000&_page=0"
+    MPurl = "http://lda.data.parliament.uk/commonsmembers.json?_view=members&_pageSize=3000&_page=0"
 
     with urllib.request.urlopen(MPurl) as url:
         MPlist = json.loads(url.read().decode())['result']['items']
@@ -15,9 +15,10 @@ def retrieve_mp_data():
     for mp in MPlist:
         constituency = mp["constituency"]["label"]["_value"]
         fullname = mp["givenName"]["_value"].strip() + " " + mp["familyName"]["_value"].strip()
+        real_fullname = mp["fullName"]["_value"].strip()
         id = (mp["_about"]).split("/")[-1]
 
-        MPdata[fullname] = [constituency]
+        MPdata[fullname] = [constituency, real_fullname]
 
     with open('190391mpl.csv') as csv_file:
         csv_reader = csv.reader(csv_file)
@@ -26,20 +27,16 @@ def retrieve_mp_data():
             try:
                 MPdata[full_name].append(row[3].replace(" ", "").replace('"', ''))
             except:
-                # print(full_name)
                 pass
 
     MPdata_formatted = []
     errors = 0
     for key, value in MPdata.items():
         try:
-            MPdata_formatted.append({"name": key, "email": value[1], "constituency": value[0]})
+            MPdata_formatted.append({"name": value[1], "email": value[2], "constituency": value[0]})
         except:
             errors += 1
-            # print(key)
             pass
-    # print(MPdata_formatted)
-    # print(errors)
     return MPdata_formatted
 
 retrieve_mp_data()
