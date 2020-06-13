47b5ca649050e55ffb9a4ef0a4c9018ccc3c6f78
Rafee <rafeejenkins@gmail.com>
2020-06-13 23:28:27 +0100

    final changes to script

--- get_ministers.py
+++ get_ministers.py
@@ -39,18 +39,33 @@ def get_minister_names():
             .contents[3]
             .text.strip()
         )
-
-    return cabinet_data
+    count = 1
+    dept_array = []
+    for department in cabinet_data.keys():
+        department_dict = {}
+        department_dict["_id"] = count
+        department_dict["name"] = department
+        department_dict["staff"] = cabinet_data[department]
+        dept_array.append(department_dict)
+        count += 1
+    return dept_array
 
 
 def get_db_id(cabinet_office):
+    array = []
     for department, minister_list in cabinet_office.items():
         for minister in minister_list:
-            query = {"name": minister}
-            id = myDb.get_one("mp_email_list", query)._id
+            department = {}
+            try:
+                id = mongo.get_one("mp_email_list", {"name": minister})["_id"]
+            except:
+                print("there has been an error with {}".format(minister))
+                id = minister
             minister_list.remove(minister)
             minister_list.append(id)
     return cabinet_office
 
 
-print(get_db_id(get_minister_names()))
+print(get_minister_names())
+
+# mongo.insert_many("mp_cabinet",set_up_id(get_db_id(get_minister_names())))
