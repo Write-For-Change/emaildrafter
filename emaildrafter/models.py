47b0dd34427d4209fda6b26b6cc9e43cbcc9b933
David Swarbrick <david.swarbrick9@gmail.com>
2020-06-28 16:38:27 +0100

    Model relations set up

--- emaildrafter/models.py
+++ emaildrafter/models.py
@@ -1,6 +1,29 @@
 from django.db import models
 
 
+class EmailTemplate(models.Model):
+    """Model for Email Templates as stored in the database."""
+
+    # https://stackoverflow.com/questions/1592291/what-is-the-email-subject-length-limit
+    subject = models.CharField(max_length=78)
+    body = models.TextField()
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(unique=True)
+    # blank = True allows these extra information fields to be empty
+    more_info = models.URLField(blank=True)
+    author_url = models.URLField(blank=True)
+    target_is_local_mp = models.BooleanField(default=True)
+    public = models.BooleanField(default=False)
+    upload_date = models.DateTimeField()
+
+    # Target field only set if NOT target_is_local_mp, stores a related target object
+    target = models.OneToOneField(
+        "SpecificTarget", on_delete=models.CASCADE, default=None
+    )
+    # Multiple topics can be associated with a template
+    topics = models.ManyToManyField("Topic")
+
+
 class EmailTarget(models.Model):
     """Abstract base class for email targets with their common required information"""
 
@@ -24,19 +47,19 @@ class SpecificTarget(EmailTarget):
     pass
 
 
-class EmailTemplate(models.Model):
-    """Model for Email Templates as stored in the database."""
+class Topic(models.Model):
+    """Store the Topics of the templates separately"""
 
-    # https://stackoverflow.com/questions/1592291/what-is-the-email-subject-length-limit
-    subject = models.CharField(max_length=78)
-    body = models.TextField()
     name = models.CharField(max_length=200)
     slug = models.SlugField()
-    # Want to have a link to a Target object, but need to discuss how to store this ie many-to-one/one-to-one etc.
-    # Will take a form similar to below: (this fails as EmailTarget is abstract)
-    # target = models.ForeignKey(EmailTarget, on_delete=models.CASCADE,)
+    more_info = models.URLField()
+    description = models.TextField()
 
 
 class EmailTemplateSubmitter(models.Model):
+    """A user of the site who submits a template or topic."""
+
     name = models.CharField(max_length=254)
     email = models.EmailField()
+    # Store the template this user has submitted. On deletion of the template, delete the user's information also.
+    template = models.OneToOneField(EmailTemplate, on_delete=models.CASCADE)
